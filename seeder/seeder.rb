f044a3a439fbcf94d4c787302a5264cb3400b1b5
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-02-11 11:53:28 +0100

    Update scraper.

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -3,7 +3,7 @@ pages << {
     page_type: 'products_search',
     method: 'GET',
 
-    url: 'https://www.willys.se/c/Dryck/Sport-och-energridryck/Energidryck?avoidCache=1549831339477&categoryPath=Dryck%2FSport-och-energridryck%2FEnergidryck&page=1&size=30',
+    url: 'https://www.willys.se/c/Dryck/Sport-och-energridryck/Energidryck?avoidCache=1549831339477&categoryPath=Dryck%2FSport-och-energridryck%2FEnergidryck&page=0&size=30',
     vars: {
         'input_type' => 'taxonomy',
         'search_term' => '-',
@@ -20,7 +20,7 @@ search_terms.each do |search_term|
   pages << {
       page_type: 'products_search',
       method: 'GET',
-      url: "https://www.willys.se/search?page=1&q=#{CGI.escape(search_term)}&size=30",
+      url: "https://www.willys.se/search?page=0&q=#{CGI.escape(search_term)}&size=30",
       vars: {
           'input_type' => 'search',
           'search_term' => search_term,
